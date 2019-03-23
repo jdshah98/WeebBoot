fa9f8ba938261e7d8dee3a92929ba46642e2cf49
Shah Jainam <31538741+jainam21@users.noreply.github.com>
2019-03-23 15:59:07 +0530

    Add files via upload

--- New folder/fooddeliverysystem.sql
+++ New folder/fooddeliverysystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2019 at 09:40 AM
+-- Generation Time: Mar 23, 2019 at 11:28 AM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -25,6 +25,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(255) NOT NULL,
+  `cid` int(255) NOT NULL,
+  `rid` int(255) NOT NULL,
+  `fid` int(255) NOT NULL,
+  `qty` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `corder`
+--
+
+CREATE TABLE `corder` (
+  `id` int(255) NOT NULL,
+  `cid` int(255) NOT NULL,
+  `vid` int(255) NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `tamt` varchar(255) NOT NULL,
+  `lat_long` varchar(255) NOT NULL,
+  `oAdd` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `customer`
 --
 
@@ -41,7 +72,8 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`cId`, `uId`, `cName`, `cMobile`) VALUES
 (1, 1, 'jainam', '8469368554'),
-(2, 2, 'Parth', '9876543210');
+(2, 2, 'Parth', '9876543210'),
+(3, 3, '', '');
 
 -- --------------------------------------------------------
 
@@ -51,12 +83,22 @@ INSERT INTO `customer` (`cId`, `uId`, `cName`, `cMobile`) VALUES
 
 CREATE TABLE `food` (
   `fid` int(10) NOT NULL,
+  `rid` int(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `ingredients` varchar(255) NOT NULL,
   `category` varchar(255) NOT NULL,
   `cost` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`fid`, `rid`, `name`, `ingredients`, `category`, `cost`, `url`) VALUES
+(1, 1, 'food1', 'This is best food', 'south', '200.00', 'food1.jpg'),
+(2, 1, 'food2', 'This is best food', 'south', '150.00', 'food2.jpg'),
+(3, 1, 'food3', 'This is best food', 'south', '170.00', 'food3.jpg');
 
 -- --------------------------------------------------------
 
@@ -68,8 +110,17 @@ CREATE TABLE `restaurant` (
   `rId` int(10) NOT NULL,
   `uId` int(10) NOT NULL,
   `rName` varchar(255) NOT NULL,
-  `rMobile` varchar(255) NOT NULL
+  `rMobile` varchar(255) NOT NULL,
+  `add` varchar(255) NOT NULL,
+  `lat_long` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`rId`, `uId`, `rName`, `rMobile`, `add`, `lat_long`) VALUES
+(1, 4, 'rest1', '7845123696', 'surat', '21.2058112,72.8547328');
 
 -- --------------------------------------------------------
 
@@ -96,7 +147,29 @@ INSERT INTO `session` (`sid`, `uid`, `uEmail`, `uType`, `uTime`, `uIp`) VALUES
 ('0fcpgis3ul6cr4su2652kc2c5c', 2, 'royparth94@gmail.com', 'customer', '1552994399', '172.16.3.1'),
 ('0fcpgis3ul6cr4su2652kc2c5c', 2, 'royparth94@gmail.com', 'customer', '1552994426', '172.16.3.1'),
 ('0fcpgis3ul6cr4su2652kc2c5c', 2, 'royparth94@gmail.com', 'customer', '1552994430', '172.16.3.1'),
-('0fcpgis3ul6cr4su2652kc2c5c', 2, 'royparth94@gmail.com', 'customer', '1552995146', '172.16.3.1');
+('0fcpgis3ul6cr4su2652kc2c5c', 2, 'royparth94@gmail.com', 'customer', '1552995146', '172.16.3.1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334688', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334694', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334710', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334710', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334711', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334712', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334712', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334712', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334712', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334753', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334753', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334753', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334753', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334754', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334754', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334754', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334754', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334754', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334754', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334755', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334755', '::1'),
+('9akmc7m2nii9kecfl1g5g7pdum', 3, '', 'customer', '1553334755', '::1');
 
 -- --------------------------------------------------------
 
@@ -117,11 +190,39 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`uId`, `uType`, `uEmail`, `uPassword`) VALUES
 (1, 'customer', 'shahjainamj8657@gmail.com', '$2y$10$EjVGmZcPqBd/zWt1YW.Hou4DlaAtJwC2WSPEzp5coOdIZBvfxin7C'),
-(2, 'customer', 'royparth94@gmail.com', '$2y$10$lwtYiUuEqm1342zrrAZgQeNMdVjnd3J9QXdO99K6ptYJ2C9Z8cbDq');
+(2, 'customer', 'royparth94@gmail.com', '$2y$10$lwtYiUuEqm1342zrrAZgQeNMdVjnd3J9QXdO99K6ptYJ2C9Z8cbDq'),
+(3, 'customer', '', '$2y$10$IX7eiQ/yBzfI6AnB.a5YBepKp6qfuwyIIGAj9UmR9oLv8p0M50Koy'),
+(4, 'restaurant', 'rest@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle`
+--
+
+CREATE TABLE `vehicle` (
+  `id` int(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `vnumber` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `cadd` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `corder`
+--
+ALTER TABLE `corder`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `customer`
@@ -148,32 +249,56 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`uId`);
 
 --
+-- Indexes for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `corder`
+--
+ALTER TABLE `corder`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `cId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `cId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `fid` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `fid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `rId` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `rId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
