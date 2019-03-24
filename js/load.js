8f34d3eaaa16359a052a427883da2ce12b2e9d54
royaldream <royparth94@gmail.com>
2019-03-24 17:58:39 +0530

    food image url set

--- js/load.js
+++ js/load.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
             var foods = JSON.parse(params);
             foods.forEach(element => {
-                var food_items = '<div class="col-md-4 item-div-' + element['fid'] + '"><div class="card shadow-lg p-0 mb-4 bg-light"><img class="card-img-top" src="./img/' + element['url'] + '"><div class="card-body"><p>' + element['name'] + '</p><p>' + element['cost'] + '</p><a href="#" onclick="itemClick(this)" class="btn btn-outline-secondary ' + element['fid'] + '">Add to Cart</a></div></div></div>';
+                var food_items = '<div class="col-md-4 item-div-' + element['fid'] + '"><div class="card shadow-lg p-0 mb-4 bg-light"><img class="card-img-top" src="./' + element['url'] + '"><div class="card-body"><p>' + element['name'] + '</p><p>' + element['cost'] + '</p><a href="#" onclick="itemClick(this)" class="btn btn-outline-secondary ' + element['fid'] + '">Add to Cart</a></div></div></div>';
                 $('.foods-menu').append(food_items);
             });
 
